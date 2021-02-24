01a44593d024de8a02c47682f812778132a47ff3
Lawrence Dovin <lawrencedovin@gmail.com>
2021-02-24 18:20:26 -0500

    Updated program to retry and added kaiser towns text file

--- airbnb-rent.py
+++ airbnb-rent.py
@@ -1,12 +1,25 @@
 from counties import cities
+import os
 
 sorted_cities = sorted(city.lower() for city in cities)
 
-answer = int(input('Alphabetical order type 1 check by town type 2 > '))
+def find_kaiser_town(sorted_cities):
+    answer = int(input('Alphabetical order type 1 check by town type 2 > '))
 
-if answer == 1:
-    for index, city in enumerate(sorted_cities):
-        print(f'{index + 1}. {city.title()}')
-if answer == 2:
-    check_city = input('What city do you want to check if it exists > ')
-    print(f'{check_city.title()} is in Kaiser Area') if check_city.lower() in sorted_cities else print(f'{check_city.title()} is NOT in Kaiser Area')
+    if answer == 1:
+        for index, city in enumerate(sorted_cities):
+            print(f'{index + 1}. {city.title()}')
+    if answer == 2:
+        check_city = input('What city do you want to check if it exists in the Kaiser Area > ')
+        print(f'{check_city.title()} is in Kaiser Area') if check_city.lower() in sorted_cities else print(f'{check_city.title()} is NOT in Kaiser Area')
+    
+    try_again = input('Type y to retry the program or n to exit > ').lower()
+
+    if(try_again == 'y'):
+        os.system('clear') # Linux - OSX
+        os.system('cls') # Windows
+        find_kaiser_town(sorted_cities)
+    else:
+        exit()
+
+find_kaiser_town(sorted_cities)
